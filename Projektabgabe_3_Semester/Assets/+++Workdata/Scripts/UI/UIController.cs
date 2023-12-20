fb500a907206d33c1f715555541c2a794147ddf1
CliveLayton <hersh-layt@freenet.de>
2023-12-20 17:29:54 +0100

    added game over screen and enemy bullet prefab

--- Projektabgabe_3_Semester/Assets/+++Workdata/Scripts/UI/UIController.cs
+++ Projektabgabe_3_Semester/Assets/+++Workdata/Scripts/UI/UIController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class UIController : MonoBehaviour
 {
     public static UIController instance;
 
-   [SerializeField] private Text text;
+   [SerializeField] private TextMeshProUGUI text;
    [SerializeField] private GameObject gameOverScreen;
    [SerializeField] private RectTransform inventoryParent;
    [SerializeField] private WeaponInventoryEntry entryPrefab;
